448310a126da0005ce8f312854ab55d7044d00eb
FesalBadday <fesal.n04@hotmail.com>
2021-11-17 13:28:54 -0700

    tweaks

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,7 +2,7 @@
   <nav class="bg-yellow-600 p-7">
     <ul class="flex justify-evenly text-2xl font-bold">
       <li><NuxtLink class="hover:text-indigo-700" to="/">Home</NuxtLink></li>
-      <li><NuxtLink class="hover:text-indigo-700" to="/Gallery">Gallery</NuxtLink></li>
+      <li><NuxtLink class="hover:text-indigo-700" to="/gallery">Gallery</NuxtLink></li>
       <li><NuxtLink class="hover:text-indigo-700" to="#">About</NuxtLink></li>
       <li><NuxtLink class="hover:text-indigo-700" to="#">Contact</NuxtLink></li>
     </ul>
